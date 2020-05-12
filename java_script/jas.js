bca434abca917328738b5f2fd9baac331e68f3a8
BINDHUMADHAVAVARMA <bindhumadhavavarma@gmail.com>
2020-05-12 23:04:42 +0530

    assign

--- java_script/jas.js
+++ java_script/jas.js
@@ -1,5 +1,11 @@
-function sayhello(){
+function sayhello(event){
   var name=document.getElementById("name").value;
   var mes="Hello "+name+" !";
   document.getElementById("content").textContent=mes;
+  this.textContent="said it";
 }
+function regenerate(event){
+  document.getElementById("button").textContent="say it";
+}
+document.querySelector("#button").addEventListener("click",sayhello);
+document.querySelector("#button").addEventListener("blur",regenerate);
