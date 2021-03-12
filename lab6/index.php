59d6dec3cd89c998ef406ee61501bac3bfe8185c
BINDHUMADHAVAVARMA <bindhumadhavavarma@gmail.com>
2021-03-12 20:13:47 +0530

    initial commit

--- lab6/index.php
+++ lab6/index.php
@@ -15,17 +15,58 @@
     <div class="details"><a href="index.php">Login/signup</a></div>
   </div>
   <div class="formcont">
-  <form action="quiz.php" method="POST"> 
+    <div id="error" style="color:red;"></div>
+  <form name="myForm" onSubmit="return(validate());" action="quiz.php" method="POST" > 
         <label for="Name">Name</label>
         <input type="text" name="name">
         <label for="Username">UserName</label>
         <input type="email" name="email">
         <label for="mobile">Mobile Number</label>
         <input type="text" name="mobile">
-        <input type="submit">
+        <input type="submit" value="Submit">
     </form>
   </div>
     
-  
+  <script type="text/javascript">
+            function validate() {
+                var l=/[a-zA-z]+$/;
+                var l1=document.myForm.name.value.match(l);
+                    if(!l1){
+                        document.getElementById("error").textContent="Name must be only characters";
+                        return false;
+                    }
+                
+                if( document.myForm.name.value == "" ) {
+                    document.getElementById("error").textContent="Please provide your name!";
+                document.myForm.Name.focus() ;
+                return false;
+                }
+                
+                if( document.myForm.email.value == "" ) {
+                    document.getElementById("error").textContent="Please provide your Email!" ;
+                document.myForm.email.focus() ;
+                return false;
+                }
+
+                if( document.myForm.mobile.value == "" || isNaN( document.myForm.mobile.value ) ||
+                document.myForm.mobile.value.length != 10 ) {
+                
+                    document.getElementById("error").textContent="Please provide a correct mobile num.";
+                document.myForm.mobile.focus() ;
+                return false;
+                }
+               
+                var emailID = document.myForm.email.value;
+                atpos = emailID.indexOf("@");
+                dotpos = emailID.lastIndexOf(".");
+                
+                if (atpos < 1 || ( dotpos - atpos < 2 )) {
+                    document.getElementById("error").textContent="Please enter correct email ID";
+                document.myForm.EMail.focus() ;
+                return false;
+                }
+                return true;
+            }
+        </script>
 </body>
 </html>
