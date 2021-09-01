dd9b74cbcba5b8c240274fb7ef1026dda52d07c7
C Bindhu Madhava Varma <54420831+BINDHUMADHAVAVARMA@users.noreply.github.com>
2021-09-01 13:19:31 +0530

    Add files via upload

---
+++ team-card/src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import TeamCard from "./components/TeamCard";
+function App() {
+  return (
+    <React.Fragment>
+      <section id="cards">
+        <div class="container py-2">
+            <div class="row pb-4">
+                <div class="col-12 text-center">
+                    <div class="display-3">Our Team</div>
+                </div>
+            </div>
+            <div class="row">
+            <TeamCard image="https://bindhumadhav.in/temp/images/1.jpg"
+             facebook="https://www.facebook.com/bindumadhava.varma/"
+             mail="mailto:bindhu.19bcd7116@vitap.ac.in"
+             linkedin="https://www.linkedin.com/in/bindhu-madhav-varma-c-58140b1aa/"
+             github="https://github.com/BINDHUMADHAVAVARMA"
+             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia
+             commodi."></TeamCard>
+              <TeamCard image="https://bindhumadhav.in/temp/images/1.jpg"
+             facebook="https://www.facebook.com/bindumadhava.varma/"
+             mail="mailto:bindhu.19bcd7116@vitap.ac.in"
+             linkedin="https://www.linkedin.com/in/bindhu-madhav-varma-c-58140b1aa/"
+             github="https://github.com/BINDHUMADHAVAVARMA"
+             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia
+             commodi."></TeamCard>
+              <TeamCard image="https://bindhumadhav.in/temp/images/1.jpg"
+             facebook="https://www.facebook.com/bindumadhava.varma/"
+             mail="mailto:bindhu.19bcd7116@vitap.ac.in"
+             linkedin="https://www.linkedin.com/in/bindhu-madhav-varma-c-58140b1aa/"
+             github="https://github.com/BINDHUMADHAVAVARMA"
+             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia
+             commodi."></TeamCard>
+              <TeamCard image="https://bindhumadhav.in/temp/images/1.jpg"
+             facebook="https://www.facebook.com/bindumadhava.varma/"
+             mail="mailto:bindhu.19bcd7116@vitap.ac.in"
+             linkedin="https://www.linkedin.com/in/bindhu-madhav-varma-c-58140b1aa/"
+             github="https://github.com/BINDHUMADHAVAVARMA"
+             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia
+             commodi."></TeamCard>
+              <TeamCard image="https://bindhumadhav.in/temp/images/1.jpg"
+             facebook="https://www.facebook.com/bindumadhava.varma/"
+             mail="mailto:bindhu.19bcd7116@vitap.ac.in"
+             linkedin="https://www.linkedin.com/in/bindhu-madhav-varma-c-58140b1aa/"
+             github="https://github.com/BINDHUMADHAVAVARMA"
+             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia
+             commodi."></TeamCard>
+            </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
