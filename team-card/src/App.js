010bc65dc81a4b585b44670fc96d67f466fc1282
BINDHUMADHAVAVARMA <bindhumadhavavarma@gmail.com>
2021-11-02 17:58:04 +0530

    nothing

--- team-card/src/App.js
+++ team-card/src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import TeamCard from "./components/TeamCard";
+const Container=styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Nunito');
+  font-style:'Nunito',sans-serif;
+  padding-bottom: .5rem;
+  padding-top: 3rem;
+  max-width:100%
+  @media (min-width: 576px){
+    max-width: 540px;
+  }
+  @media (min-width: 768px){
+    max-width: 720px;
+  }
+  @media (min-width: 992px){
+    max-width: 960px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+const Row=styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`;
 function App() {
   return (
     <React.Fragment>
-      <section id="cards">
-        <div class="container py-2">
-            <div class="row pb-4">
-                <div class="col-12 text-center">
-                    <div class="display-3">Our Team</div>
-                </div>
-            </div>
-            <div class="row">
+      <section>
+        <Container>
+            <Row>
             <TeamCard image="https://bindhumadhav.in/temp/images/1.jpg"
              facebook="https://www.facebook.com/bindumadhava.varma/"
              mail="mailto:bindhu.19bcd7116@vitap.ac.in"
@@ -46,8 +72,8 @@ function App() {
              github="https://github.com/BINDHUMADHAVAVARMA"
              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia
              commodi."></TeamCard>
-            </div>
-        </div>
+            </Row>
+        </Container>
       </section>
     </React.Fragment>
   );
--- team-card/src/components/TeamCard.js
+++ team-card/src/components/TeamCard.js
@@ -1,26 +1,111 @@
-import  "../bootstrap.min.css";
-import "./TeamCard.css";
+
+import styled from 'styled-components';
+
+const TeamCardWrapper=styled.div`
+max-width:90%;
+@media (min-width: 610px){
+    max-width:80%;
+  }
+  @media (min-width: 768px){
+    width: 45%;
+  }
+  @media (min-width: 992px){
+    width: 30%;
+  }
+    margin:20px auto;
+`;
+const Card=styled.div`
+    font-falmily:'Nunito',sans-serif;
+    position:relative;
+    border-radius: 20px;
+    min-height: 350px;
+    
+    font-size: large;
+    box-shadow: 0 1rem 3rem rgba(0, 0, 0, .175);
+    & .user-picture {
+        position: absolute;
+        height:100%;
+        width:100%;
+        top: 0;
+        right: 0;
+        background-size: cover;
+        background-position: center top;
+        background-repeat: no-repeat;
+        border-radius: 20px;
+        transition: all 0.5s ease-in-out;
+    }
+    &:hover .user-picture{
+        position: absolute;
+        z-index:2;
+        top: -40px;
+        right: 30px;
+        padding: 10px;
+        height: 100px;
+        width: 100px;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        border-radius: 60px;
+    } 
+    & .description{
+        font-size:17px;
+        font-weight:600;
+        color: rgb(82, 81, 81);
+    }
+    h4{
+        font-size:1.5rem;
+        margin-bottom: .5rem;
+        font-weight: 700;
+        line-height: 1.2;
+        margin-top:0;
+    }
+    & .role{
+        font-size:18px;
+        font-weight:800;
+        margin-top:0;
+    }
+    & .icon{
+        color: black;
+        padding: 12px;
+        width: 19px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 50%;
+        transition: all 500ms;
+    }
+    & .icon:hover{
+        color:white;
+        background-color:black;
+    }
+    & .user-name{
+        max-width:60%;
+    }
+    
+`;
+const CardBody=styled.div`
+    padding:25px;
+`;
+
 function TeamCard(props){
     return(
-        <div className="col-lg-4 col-md-6 mb-4 pt-5">
-                    <div className="card shadow-lg border-0" data-aos="zoom-out" data-aos-delay="100">
-                        <div className="card-body">
-                            <div className="user-picture" style={{backgroundImage: `url(${props.image})`}}></div>
-                            <div className="user-content">
-                                <h5 className="text-capitalize user-name">Carry Johnshon</h5>
-                                <p className=" text-capitalize text-muted small blockquote-footer">Web developer</p>
-                                <div>
-                                    <a target="_blank" rel="noreferrer" href={props.facebook}><i className="fab fa-facebook-f"></i></a>
-                                    <a target="_blank" rel="noreferrer" href={props.mail}><i className="fas fa-envelope"></i></a>
-                                    <a target="_blank" rel="noreferrer" href={props.linkedin}><i className="fab fa-linkedin-in"></i></a>
-                                    <a target="_blank" rel="noreferrer" href={props.github}><i className="fab fa-github"></i></a>
-                                </div>
-                                <p className="small text-muted mb-0">{props.description}
-                                </p>
-                            </div>
+        <TeamCardWrapper>
+            <Card>
+                <CardBody>
+                    <div className="user-picture" style={{backgroundImage: `url(${props.image})`}}></div>
+                    <div className="user-content">
+                        <h4 className="user-name">Carry Johnshon</h4>
+                        <p className="role">Web developer</p>
+                        <div>
+                            <a target="_blank" rel="noreferrer" href={props.facebook}><i className="icon fab fa-facebook-f"></i></a>
+                            <a target="_blank" rel="noreferrer" href={props.mail}><i className="icon fas fa-envelope"></i></a>
+                            <a target="_blank" rel="noreferrer" href={props.linkedin}><i className="icon fab fa-linkedin-in"></i></a>
+                            <a target="_blank" rel="noreferrer" href={props.github}><i className="icon fab fa-github"></i></a>
                         </div>
+                        <p className="description">{props.description}</p>
                     </div>
-                </div>
+                </CardBody>
+            </Card>
+        </TeamCardWrapper>
     );
 }
 export default TeamCard;
