ade55d6520859ca9f103af84ab3a91a95baf8a6b
bindhumadhavavarma <bindhumadhavavarma@gmail.com>
2021-11-02 18:41:41 +0530

    fixed bug in profile page

--- js/main.js
+++ js/main.js
@@ -61,27 +61,27 @@ function update() {
         temp = 5;
     }
     if (temp == 0) {
-        document.getElementById('m1').innerHTML = '<a href="/profilev2.0/education.html"><i class="fas fa-chevron-right"></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/profilev2.0/trainings.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/education.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/trainings.html"><i class="fas fa-chevron-left"></i></i></i></a>';
     }
     if (temp == 1) {
-        document.getElementById('m1').innerHTML = '<a href="/profilev2.0/skills.html"><i class="fas fa-chevron-right"></i></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/profilev2.0/index.html"><i class="fas fa-chevron-left"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/skills.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/index.html"><i class="fas fa-chevron-left"></i></i></a>';
     }
     if (temp == 2) {
-        document.getElementById('m1').innerHTML = '<a href="/profilev2.0/internships.html"><i class="fas fa-chevron-right"></i></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/profilev2.0/education.html"><i class="fas fa-chevron-left"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/internships.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/education.html"><i class="fas fa-chevron-left"></i></i></a>';
     }
     if (temp == 3) {
-        document.getElementById('m1').innerHTML = '<a href="/profilev2.0/projects.html"><i class="fas fa-chevron-right"></i></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/profilev2.0/skills.html"><i class="fas fa-chevron-left"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/projects.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/skills.html"><i class="fas fa-chevron-left"></i></i></a>';
     }
     if (temp == 4) {
-        document.getElementById('m1').innerHTML = '<a href="/profilev2.0/trainings.html"><i class="fas fa-chevron-right"></i></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/profilev2.0/internships.html"><i class="fas fa-chevron-left"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/trainings.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/internships.html"><i class="fas fa-chevron-left"></i></i></a>';
     }
     if (temp == 5) {
-        document.getElementById('m1').innerHTML = '<a href="/profilev2.0/index.html"><i class="fas fa-chevron-right"></i></i></i></a>';
-        document.getElementById('m2').innerHTML = '<a href="/profilev2.0/projects.html"><i class="fas fa-chevron-left"></i></i></a>';
+        document.getElementById('m1').innerHTML = '<a href="/index.html"><i class="fas fa-chevron-right"></i></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="/projects.html"><i class="fas fa-chevron-left"></i></i></a>';
     }
 }
